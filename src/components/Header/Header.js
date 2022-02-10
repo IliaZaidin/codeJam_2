eb0f25513b30ddb5ecb8fcaba49e85c4c9b3ee89
Eduard Loktev <lwqtbdwrd@gmail.com>
2022-02-10 18:22:25 +0200

    add : Header , Card components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export function Header() {
+    return (
+        <header className="header">
+            <h1 className='header__title'>Valentine Card Maker</h1>
+        </header>
+    )
+
+}
